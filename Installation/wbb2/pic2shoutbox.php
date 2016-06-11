8b5c6b883cc77c1a78f1d054fb2f5a91a19f647b
kill0rz <kill0rz@users.noreply.github.com>
2016-06-11 15:40:58 +0200

    fixed randompic

--- Installation/wbb2/pic2shoutbox.php
+++ Installation/wbb2/pic2shoutbox.php
@@ -29,9 +29,11 @@ $phpversion = phpversion();
 require './acp/lib/config.inc.php';
 require './acp/lib/class_db_mysql.php';
 require './acp/lib/class_parse.php';
-// require('./acp/lib/class_parsecode.php');
+require('./acp/lib/class_parsecode.php');
 require './acp/lib/options.inc.php';
-include './picupload_functions.php';
+require './picupload_config.php';
+
+$db = new db($sqlhost, $sqluser, $sqlpassword, $sqldb, $phpversion);
 
 // -------
 
@@ -100,6 +102,8 @@ if (rand(0, 1) == 1) {
 
 if (!count($useralbums) > 0) {
 	die("Keine Alben zum indexieren!");
+}else{
+	echo count($useralbums);
 }
 
 $result = $db->unbuffered_query("SELECT username FROM bb1_users WHERE userid='{$userid}'");
